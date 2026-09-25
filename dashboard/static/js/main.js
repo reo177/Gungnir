2c6013a911caaefcc8208e703d6a56e29d3ed8d9
reika <oos.reodir@gmail.com>
2026-09-26 05:25:40 +0900

    Delete dashboard directory

--- dashboard/static/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.alert').forEach(el => {
-  setTimeout(() => {
-    el.style.transition = 'opacity 0.5s';
-    el.style.opacity = '0';
-    setTimeout(() => el.remove(), 500);
-  }, 4000);
-});
